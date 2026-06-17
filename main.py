8f474442fb4185b92d66fe251e8527dd17212dfc
sanazhasanpour <sanazhasanpour1987@gmail.com>
2026-06-17 19:40:44 +0200

    Add BMI calculation

---
+++ main.py
@@ -0,0 +1,6 @@
+weight=float(input("enter your weight in kg: "))
+height=float(input("enter your height in cm: "))
+height=height/100
+BMI=weight/(height*height)
+print("your BMI is:",round(BMI,2))
+
